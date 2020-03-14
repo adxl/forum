0cfd73c27ad9c0ca80208156080c331463ba2fed
Adel Senhadji <adelsenhadjii@gmail.com>
2020-03-14 21:23:48 +0100

    create User model

---
+++ src/main/java/com/adxl/forum/model/User.java
@@ -0,0 +1,19 @@
+package com.adxl.forum.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+
+	public Long getId(){
+		return id;
+	}
+
+}
