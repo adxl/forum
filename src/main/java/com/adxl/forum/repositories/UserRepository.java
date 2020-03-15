c989507fcc05f274c31fada61edfe4cfb00e99d0
Adel Senhadji <adelsenhadjii@gmail.com>
2020-03-15 20:36:29 +0100

    project restart

--- src/main/java/com/adxl/forum/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.adxl.forum.repositories;
-
-import com.adxl.forum.models.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User,Long> {
-}
