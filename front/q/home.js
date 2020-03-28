b2878e6425862e9ab329405d70dfdd079b3c8595
Adel Senhadji <adelsenhadjii@gmail.com>
2020-03-28 23:10:44 +0100

    add thread api call

--- front/q/home.js
+++ front/q/home.js
@@ -2,23 +2,25 @@ var home = angular.module('home', []);
 
 home.component('home', {
   templateUrl: 'q/home.html',
-  controller: function($http, $scope) {
-    $http({
-      method: 'GET',
-      url: 'http://localhost:8080/q/'
-    }).then(
-      function success(response) {
-        $scope.thread_list = response.data;
-      },
-      function error(response) {
-        console.log('error', response);
-      }
-    );
-  }
+  controller: 'homeController'
 });
 
-app.filter('limitToWords', function() {
-  return function(string, limit) {
+home.controller('homeController', function ($scope, $http) {
+  $http({
+    method: 'GET',
+    url: 'http://localhost:8080/q/'
+  }).then(
+    function success(response) {
+      $scope.thread_list = response.data;
+    },
+    function error(response) {
+      console.log('error', response);
+    }
+  );
+});
+
+app.filter('limitToWords', function () {
+  return function (string, limit) {
     if (limit) {
       return string
         .split(' ')
--- front/q/thread.js
+++ front/q/thread.js
@@ -1,8 +1,26 @@
-var thread = angular.module('thread', []);
+angular.module('thread', [])
 
-thread.component('thread', {
-  templateUrl: 'q/thread.html',
-  controller: function($scope, $routeParams) {
-    $scope.id = $routeParams.id;
-  }
-});
+	.component('thread', {
+		templateUrl: 'q/thread.html',
+		controller: 'threadController'
+	})
+
+	.controller('threadController',
+		function ($scope, $http, $routeParams) {
+			var id = $routeParams.id;
+			console.log(id);
+
+			$http
+				.get('http://localhost:8080/q/' + id)
+				.then(
+					function success(response) {
+						$scope.id = response.data.id;
+						$scope.title = response.data.question.title;
+						$scope.text = response.data.question.text;
+					},
+					function error(response) {
+						console.log('error', response);
+					}
+				);
+		}
+	);
